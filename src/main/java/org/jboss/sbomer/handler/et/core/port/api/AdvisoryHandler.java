e2a3575bdc16d0733c65ebd3758f8b306531bdbe
Mert Bicak <mbicak@redhat.com>
2025-11-12 16:17:43 +0100

    build: import optimization and automation

--- src/main/java/org/jboss/sbomer/handler/et/core/port/api/AdvisoryHandler.java
+++ src/main/java/org/jboss/sbomer/handler/et/core/port/api/AdvisoryHandler.java
@@ -1,8 +1,5 @@
 package org.jboss.sbomer.handler.et.core.port.api;
 
-import java.util.List;
-
-import org.jboss.sbomer.handler.et.core.domain.generation.Generation;
 import org.jboss.sbomer.handler.et.core.domain.generation.GenerationRequest;
 
 /**
